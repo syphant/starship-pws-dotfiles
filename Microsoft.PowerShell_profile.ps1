272f9753ed14ce12a218f30ca422c972251f5f12
syphant <88693878+syphant@users.noreply.github.com>
2023-01-14 16:54:16 -0500

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+# C:\Users\{USERNAME}\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+$ENV:STARSHIP_CONFIG = "$HOME\.starship\starship.toml"
+$ENV:STARSHIP_DISTRO = "者 ccx@eleven "
+Invoke-Expression (&starship init powershell)
